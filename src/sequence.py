845fda892aa137afc95e66f3e8471954e0cf92e7
Ready1337 <egor.zinovjev2013@yandex.ru>
2021-12-23 21:12:12 +0500

    Added opportunity to find cycles

--- src/sequence.py
+++ src/sequence.py
@@ -27,3 +27,32 @@ def calculate_sequences(sequence_setter):
             y_coords.append(y)
 
         plot_sequence(x_coords, y_coords)
+
+
+def find_cycle(sequence_setter, epsilon):
+    start = sequence_setter.get_starting_points()
+    parameter = sequence_setter.get_parameter()
+    precision = sequence_setter.get_precision()
+
+    for j in range(len(start)):
+        x_coords = [start[j][0]]
+        y_coords = [start[j][1]]
+
+        for i in range(10000):
+            point = (x_coords[i], y_coords[i])
+            x, y = runge_kutta_model(point, precision, parameter)
+
+            for z in range(len(x_coords)):
+                if abs(x_coords[z] - x) < epsilon and abs(y_coords[z] - y) < epsilon:
+                    print('cycle ok')
+                    print(f'#1: {(x, y)}')
+                    print(f'#2: {(x_coords[z], y_coords[z])}')
+                    print(f'start = {z}')
+                    print(f'end = {i}')
+                    plot_sequence(x_coords, y_coords)
+                    return
+
+            x_coords.append(x)
+            y_coords.append(y)
+
+        plot_sequence(x_coords, y_coords)
