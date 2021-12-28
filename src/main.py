e984f3154499e3d8fe9f8e05b21db4d3e1a89115
Ready1337 <egor.zinovjev2013@yandex.ru>
2021-12-28 19:38:27 +0500

    Added t_dependence plot

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from sequence import calculate_sequences, find_cycle
-from plot import show_sequence
+from plot import show_sequence, show_t_dependence
 
 
 class SequencesSetter:
@@ -40,12 +40,16 @@ if __name__ == '__main__':
     # ]
 
     # фазовый портрет - неустойчивый фокус 1.5
+    # starting_points = [
+    #     (1.5, 3)
+    # ]
+
     starting_points = [
         # (1.0994022877585143, -1.3004596127658992) # 1.1   0.001
         # (1.2006580521359789, -1.7352321631882393) # 1.2   0.001
         # (0.7115819519055993, 1.2709935477389327)  # 1.3   0.01
         # (1.40824472794171, 1.9018139579166402)    # 1.4   0.01
-        # (1.4909152575156248, 1.9945702681119177)  # 1.5   0.01
+        # (1.4816971580550782, 1.99519100039137)  # 1.5   0.01
         # (1.6090103469033865, 2.0234570512169685)  # 1.6   0.01
         # (1.7036760893009686, 1.9512500153349224)  # 1.7   0.01
         # (1.7990104300997234, 1.744160174889162)   # 1.8   0.01
@@ -70,20 +74,22 @@ if __name__ == '__main__':
 
     # фазовый портрет - устойчивый узел 10
     # starting_points = [
-    #     (10, 0.01),
-    #     (10, -0.01),
-    #     (10 + 0.0001, -0.01),
-    #     (10 - 0.0001, 0.01)
+    #     (10, 0.1),
+    #     (10, -0.1),
+    #     (9.9, 0.1),
+    #     (9.9, -0.1),
+    #     (9.95, 0.1),
+    #     (9.95, -0.1)
     # ]
 
     # eulers_example - метод Эйлера для примера
     # eulers_model - метод Эйлера для модели
     # runge_kutta_example - метод Рунге-Кутта 4-го порядка для примера
     # runge_kutta_model - метод Рунге-Кутта 4-го порядка для модели
-    sequence_setter = SequencesSetter(starting_points, parameter=1.8, precision=0.01, method_name='runge_kutta_model')
+    sequence_setter = SequencesSetter(starting_points, parameter=1.9, precision=0.01, method_name='runge_kutta_model')
 
-    # calculate_sequences(sequence_setter) !!!
-    find_cycle(sequence_setter, 0.01)
+    #calculate_sequences(sequence_setter)
+    #find_cycle(sequence_setter, 0.001)
 
     # точки предельных циклов
     # (1.0994022877585143, -1.3004596127658992) # 1.1   0.001
@@ -96,4 +102,5 @@ if __name__ == '__main__':
     # (1.7990104300997234, 1.744160174889162)   # 1.8   0.01
     # (1.900597712241486, 1.3004596127659003)   # 1.9   0.001
 
-    show_sequence()
+    #show_sequence()
+    #show_t_dependence()
--- src/plot.py
+++ src/plot.py
@@ -10,3 +10,16 @@ def show_sequence():
     plt.ylabel('y')
 
     plt.show()
+
+
+def show_t_dependence():
+    t = [3.14, 3.14, 3.14, 3.14, 3.14, 3.14, 3.14, 3.14, 3.14]
+    param = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
+
+    plt.plot(param, t)
+    plt.scatter(param, t)
+
+    plt.xlabel("parameter")
+    plt.ylabel("T")
+
+    plt.show()
